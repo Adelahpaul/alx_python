1b913f172f379156e984511093b7d1090e451597
Adelahpaul <pauladelah1@gmail.com>
2023-07-22 22:30:23 +0100

    4 update

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,12 +1,9 @@
 def fibonacci_sequence(n):
-    if n <= 0:
-     return []
-    if n == 1:
-     return [0]
-    result = [0, 1]
-    if n == 2:
-     return result
-    for i in range(2, n):
-     result.append(result[i-1] + result[i-2])
-     return result
-     
+    if n == 0:
+        return [0]
+    elif n == 1:
+        return [0, 1]
+    else:
+        lst = fibonacci_sequence(n-1)
+        lst.append(lst[-1] + lst[-2])
+        return lst
