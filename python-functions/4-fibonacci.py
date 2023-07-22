0e16a5de0cbdb648851ec51658e0a461e90b831f
Adelahpaul <pauladelah1@gmail.com>
2023-07-22 22:44:56 +0100

    changes

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,9 +1,11 @@
 def fibonacci_sequence(n):
-    if n == 0:
-        return [0]
-    elif n == 1:
-        return [0, 1]
-    else:
-        lst = fibonacci_sequence(n-1)
-        lst.append(lst[-1] + lst[-2])
-        return lst
+    if n <= 0:
+     return []
+    if n == 1:
+     return [0]
+    result = [0, 1]
+    if n == 2:
+     return result
+    for i in range(2, n):
+     result.append(result[i-1] + result[i-2])
+     return result
--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,8 +1,8 @@
 def is_prime(number):
     a = False
     b = True
-    if number % 2 ==0:
-        print(a)
-    else:
+    if number % 2>0:
         print(b)
+    elif number %2==0:
+        print(a)
         return number
