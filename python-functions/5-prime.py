f932efe1000886b7925b378bce794c1fd34c7608
Adelahpaul <pauladelah1@gmail.com>
2023-07-22 23:31:33 +0100

    ok

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,5 +1,9 @@
 def is_prime(number):
-    for n in range(2,int(number**0.5)+1):
+    if number==2 or number==3:
+        return True
+    if number%2==0 or number<2:
+        return False
+    for n in range(3,int(number**0.5)+1,2):   
         if number%n==0:
-            return False
+            return False   
     return True
