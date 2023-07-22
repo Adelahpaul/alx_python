598b01c76c19cbb820d51de46136d94e93d6703d
Adelahpaul <pauladelah1@gmail.com>
2023-07-22 22:10:02 +0100

    4 update

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,11 +1,31 @@
 def fibonacci_sequence(n):
-    if n <= 0:
-     return []
-    if n == 1:
-     return [0]
-    result = [0, 1]
-    if n == 2:
-     return result
-    for i in range(2, n):
-     result.append(result[i-1] + result[i-2])
-     return result
+    #Initial variable for the base case. 
+    first = 0
+    second = 1
+
+    #Printing the initial Fibonacci number.
+    print(first, second, end=" ")
+
+    #decreasing the length by two because the first 2 Fibonacci numbers 
+    #already printed.
+    n -= 2
+    
+    #Loop until the length becomes 0.
+    while n > 0:
+
+        #Printing the next Fibonacci number.
+        print(first + second, end=" ")
+
+        #Updating the first and second variables for finding the next number. 
+        temp = second
+        second = first + second
+        first = temp
+
+        #Decreasing the length that states the Fibonacci numbers to be 
+        #printed more.
+        n -= 1
+
+if __name__ == "__main__":
+    print("Fibonacci Series - ")
+    fibonacci_sequence(7)
+    pass
