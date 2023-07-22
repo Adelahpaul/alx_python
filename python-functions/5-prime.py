dc280f7bd324abbe3b877e9844b1aebca3654d45
Adelahpaul <pauladelah1@gmail.com>
2023-07-22 22:57:00 +0100

    good

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -2,7 +2,7 @@ def is_prime(number):
     a = False
     b = True
     if number % 2>0:
-        print(b)
-    elif number %2==0:
-        print(a)
+        print(is_prime(b))
+    else:
+        print (is_prime(a))
         return number
