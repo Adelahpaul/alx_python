b429e799c3e6724cd462d910d4b5ee4f32e7b772
Adelahpaul <pauladelah1@gmail.com>
2023-07-23 09:23:17 +0100

    please

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,6 +1,5 @@
 def validate_password(password):
-    #list = ['!', '@', '#', '$', '%', '&', '(', ')', '-', '_', '[', ']', '{', '}', ';', ':', '"', '.', '/', '<', '>', '?']
-    if any(i.isupper() for i in password) and any(i.islower() for i in password) and any(i.isdigit() for i in password) and len(password) >= 8:
-        estr = True
+    if len(password) >= 8 and any(i.isupper() for i in password) and any(i.islower() for i in password) and any(i.isdigit() for i in password):
+        return True
     else:
         return False
