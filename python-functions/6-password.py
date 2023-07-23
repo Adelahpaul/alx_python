9532f5aa1f86e766c1483d5f17dce547c36b164c
Adelahpaul <pauladelah1@gmail.com>
2023-07-23 10:30:54 +0100

    changes

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,7 +1,7 @@
 def validate_password(password):
     if len(password) >= 8 and any(i.isupper() for i in password) and  any(i.islower() for i in password) and any(i.isdigit() for i in password):
         return True
-    elif ('' in password):
+    elif (i.isspace() for i in password):
         return False
     else:
         return False
