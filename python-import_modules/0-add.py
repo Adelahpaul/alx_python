72775f3773463feb747d8ddc25ba1bf686010497
Adelahpaul <pauladelah1@gmail.com>
2023-07-24 21:00:14 +0100

    update task zero

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 from add_0 import add
-
 a = 1 
 b = 2
-
-if __name__ == "__name__":
-    print("{}+{} = {}" .format(a,b, add(a,b)))
+def main():
+    print("{}+{} = {}" .format(a,b, add(a,b))) 
+    if __name__ == "__name__":
+        main()
