1de0a61b2745bba37b86594dc963c3bc132607c6
Adelahpaul <pauladelah1@gmail.com>
2023-07-27 21:08:16 +0100

    solved

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,27 +1,8 @@
  #!/usr/bin/env python3
 def validate_password(password):
-    if len(password) < 8:
+    if len(password) >= 8 and any(i.isupper() for i in password) and  any(i.islower() for i in password) and any(i.isdigit() for i in password):
+        return True
+    elif (i.isspace() for i in password):
         return False
-    has_uppercase = False
-    has_lowercase = False
-    has_digit = False
-
-    for char in password:
-        if char.isupper():
-            has_uppercase = True
-        elif char.islower():
-            has_lowercase = True
-        elif char.isdigit():
-            has_digit = True
-
-    if not (has_uppercase and has_lowercase and has_digit):
+    else:
         return False
-
-    # Check for spaces
-    if ' ' in password:
-        return False
-
-    return True
-print(validate_password("Abc123def"))
-print(validate_password("password"))    
-print(validate_password("Short1"))      
