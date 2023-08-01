0cda035ec71f19949365edf067ecc337fbc8ba12
Adelahpaul <pauladelah1@gmail.com>
2023-08-01 22:43:14 +0100

    work done

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+"""Class Square that defines a square.
+    """
+
+
 class Square:
+    """Class Square that defines a square.
+    """
     pass
 
-size = Square()
-size.val1
-size.val2
-print(size.val1 * size.val2) 
