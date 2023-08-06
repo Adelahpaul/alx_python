cbc433e845d573223f01130e1b6344463aa84d6b
Adelahpaul <pauladelah1@gmail.com>
2023-08-06 21:01:06 +0100

    task two

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -11,6 +11,10 @@ class Square:
     Private instance attributes: size
     """
 
-    def __init__(self, size):
+    def __init__(self, size = 0):
         """Initializes size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
